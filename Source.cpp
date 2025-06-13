c5d2c814fcceeb7a088cb9bc3e2c3f79659627b2
DarinDmitry1 <dima.darin.2023@gmail.com>
2025-06-13 20:41:30 +0500

    Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -12,6 +12,10 @@ string Comment::str() const {
     return text + " (Likes: " + to_string(likes) + ")";
 }
 
+bool isUint(const string& s) {
+    return s.find_first_not_of("0123456789") == string::npos;
+}
+
 Song::Song(const string& grp, const string& ttl, int bpmVal, const string& k)
     : group(grp), title(ttl), bpm(bpmVal), key(k) {}
 
@@ -52,59 +56,64 @@ SongManager::SongManager(const string& file) : filename(file) {
 void SongManager::loadFromFile() {
     ifstream fin(filename);
     if (!fin) {
-        cout << "Файл не найден. Создается новый при сохранении.\n";
+        cout << "Файл не найден. Будет создан при сохранении.\n";
         return;
     }
 
     string line;
-    while (getline(fin, line)) {
-        stringstream ss(line);
-        string group, title, bpmStr, key;
+    Song* currentSong = nullptr;
 
-        if (getline(ss, group, ';') &&
-            getline(ss, title, ';') &&
-            getline(ss, bpmStr, ';') &&
-            getline(ss, key, ';')) {
-            try {
-                int bpm = stoi(bpmStr);
-                songs.emplace_back(group, title, bpm, key);
+    while (getline(fin, line)) {
+        if (line == "---") {
+            currentSong = nullptr;
+            continue;
+        }
+
+        if (line.rfind("#COMMENT:", 0) == 0 && currentSong) {
+            string commentLine = line.substr(9);
+            size_t sep = commentLine.rfind(';');
+            if (sep != string::npos) {
+                string text = commentLine.substr(0, sep);
+                int likes = stoi(commentLine.substr(sep + 1));
+                Comment c(text);
+                c.likes = likes;
+                currentSong->comments.push_back(c);
             }
-            catch (...) {
-                cout << "Ошибка чтения строки: " << line << endl;
+        }
+        else {
+            stringstream ss(line);
+            string group, title, bpmStr, key;
+
+            if (getline(ss, group, ';') &&
+                getline(ss, title, ';') &&
+                getline(ss, bpmStr, ';') &&
+                getline(ss, key, ';')) {
+
+                try {
+                    int bpm = stoi(bpmStr);
+                    songs.emplace_back(group, title, bpm, key);
+                    currentSong = &songs.back();    
+                }
+                catch (...) {
+                    cout << "Ошибка чтения строки: " << line << endl;
+                }
             }
         }
     }
 }
 
+
 void SongManager::saveToFile() {
     ofstream fout(filename);
     for (const auto& song : songs) {
         fout << song.group << ";" << song.title << ";" << song.bpm << ";" << song.key << ";\n";
-    }
-}
 
-void SongManager::removeSong() {
-    int id;
-    cout << "Введите ID песни для удаления: ";
-    cin >> id;
+        for (const auto& comment : song.comments) {
+            fout << "#COMMENT:" << comment.text << ";" << comment.likes << "\n";
+        }
 
-    if (id < 0 || id >= songs.size()) {
-        cout << "Неверный ID.\n";
-        return;
+        fout << "---\n";
     }
-
-    cout << "Вы уверены, что хотите удалить песню \"" << songs[id].title << "\"? (y/n): ";
-    char ch;
-    cin >> ch;
-    if (ch == 'y' || ch == 'Y') {
-        songs.erase(songs.begin() + id);
-        cout << "Песня удалена.\n";
-    }
-    else {
-        cout << "Удаление отменено.\n";
-    }
-
-    cin.ignore();
 }
 
 
@@ -126,7 +135,6 @@ void SongManager::displaySongs() const {
             << " | " << setw(6) << left << songs[i].key << endl;
     }
 }
-
 
 void SongManager::editSong() {
     int id;
@@ -152,10 +160,10 @@ void SongManager::editSong() {
 }
 
 void SongManager::commentMenu() {
+    string klmn;
     int id;
     cout << "Введите ID песни для просмотра комментариев: ";
-    cin >> id;
-    cin.ignore();
+    id = getSimpleInt(klmn);
 
     if (id < 0 || id >= songs.size()) {
         cout << "Неверный ID.\n";
@@ -164,13 +172,14 @@ void SongManager::commentMenu() {
 
     Song& song = songs[id];
     int choice;
+    string asd;
     do {
         clearScreen();
         cout << "Комментарии к песне: " << song.title << "\n";
         song.displayComments();
         cout << "\n1. Добавить комментарий\n2. Лайкнуть комментарий\n0. Назад\nВыбор: ";
-        cin >> choice;
-        cin.ignore();
+
+        choice = getSimpleInt(asd);
 
       switch(choice){
         case 1:{
@@ -187,11 +196,13 @@ void SongManager::commentMenu() {
             song.likeComment(idx);
             break;
         }
-        case 3: {
-
+        case 0: {
+            clearScreen();
+            break;
         }
         default: {
             cout << "Ошибка, неверный выбор" << endl;
+            break;
         }
       }
     } while (choice != 0);
@@ -199,6 +210,7 @@ void SongManager::commentMenu() {
 
 void SongManager::menu() {
     int choice;
+    string dk;
     do {
         cout << "1. Показать все песни\n"
             << "2. Редактировать песню\n"
@@ -208,9 +220,9 @@ void SongManager::menu() {
             << "6. Удалить песню\n"
             << "0. Выход\n"
             << "Выбор: ";
-        cin >> choice;
-        cin.ignore();
 
+        choice = getSimpleInt(dk);
+     
         switch (choice) {
         case 1: clearScreen(); displaySongs(); break;
         case 2: clearScreen(); editSong(); break;
@@ -225,7 +237,7 @@ void SongManager::menu() {
 }
 
 
-    void SongManager::addNewSong() {
+void SongManager::addNewSong() {
     string group, title, key;
     int bpm;
 
@@ -252,4 +264,44 @@ void SongManager::menu() {
 #else
         std::system("clear");
 #endif
+    }
+
+    void SongManager::removeSong() {
+        int id;
+        cout << "Введите ID песни для удаления: ";
+        cin >> id;
+
+        if (id < 0 || id >= songs.size()) {
+            cout << "Неверный ID.\n";
+            return;
+        }
+
+        cout << "Вы уверены, что хотите удалить песню \"" << songs[id].title << "\"? (y/n): ";
+        char ch;
+        cin >> ch;
+        if (ch == 'y' || ch == 'Y') {
+            songs.erase(songs.begin() + id);
+            cout << "Песня удалена.\n";
+        }
+        else {
+            cout << "Удаление отменено.\n";
+        }
+
+        cin.ignore();
+    }
+
+    int getSimpleInt(const string& prompt) {
+        string input;
+        int value;
+
+        while (true) {
+            cout << prompt;
+            getline(cin, input);
+            if (!input.empty() && all_of(input.begin(), input.end(), ::isdigit)) {
+                value = stoi(input);
+                return value;
+            }
+
+            cout << "Введите только целое число.\n";
+        }
     }
